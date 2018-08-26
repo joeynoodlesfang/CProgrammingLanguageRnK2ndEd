8c64a3cb2b732a251bd388942525c62f3347d24b
Joe <zhoufang29@gmail.com>
2018-08-26 11:11:52 -0700

    edited solutions for 1-16

--- Ch1/1-16/1-16.c
+++ Ch1/1-16/1-16.c
@@ -1,28 +1,28 @@
 /**
- * 1-16: getLine function that prints longest line + line count
+ * 1-16: getLineWithLength function that prints longest line + line count
  */
 
 #include <stdio.h>
 #define MAXLINE 1000
 
-int getLine(FILE *fp, char line[], int maxline);
-void copy(char to[], char from[]);
-/* print the longest input line */
+int getLineWithLength(FILE *fp, char line[], int maxline);
+void copyLongest(char to[], char from[]);
+
 int main() {
-	int len; /* current line length */
-	int max; /* maximum length seen so far */
-	char line[MAXLINE]; /* current input line */
-	char longest[MAXLINE]; /* longest line saved here */
+	int len;
+	int max;
+	char line[MAXLINE];
+	char longest[MAXLINE];
 	max = 0;
 	FILE *fp = fopen("T:\\Take\\CProgrammingLanguageRnK2ndEd\\Ch1\\1-16\\test.txt", "r");
 	if (fp == NULL) {
 		perror("wow!");
 		return -1;
 	}
-	while ((len = getLine(fp, line, MAXLINE)) > 0)
+	while ((len = getLineWithLength(fp, line, MAXLINE)) > 0)
 		if (len > max) {
 			max = len;
-			copy(longest, line);
+			copyLongest(longest, line);
 		}
 	if (max > 0) /* there was a line */
 		printf("max leng = %d: %s", max, longest);
@@ -30,8 +30,8 @@ int main() {
 	return 0;
 }
 
-/* getLine: read a line into s, return length */
-int getLine(FILE* fp, char s[], int lim) {
+
+int getLineWithLength(FILE* fp, char s[], int lim) {
 	int c, i;
 	c = i = 0;
 
@@ -45,8 +45,9 @@ int getLine(FILE* fp, char s[], int lim) {
 	s[i] = '\0';
 	return i;
 }
-/* copy: copy 'from' into 'to'; assume to is big enough */
-void copy(char to[], char from[]) {
+
+
+void copyLongest(char to[], char from[]) {
 	int i;
 	i = 0;
 	while ((to[i] = from[i]) != '\0')
