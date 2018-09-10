764fc35b5dc483b94d2a3fedaeecbc5d8677378e
Joe <zhoufang29@gmail.com>
2018-09-09 23:14:49 -0700

    Added 1-17, corrected 1-16 bug with line count. 1-16 had a bug where last line count is wrong, due to poor method originally of counting. Originally, newline character counted as a letter, and count value was subtracted by one to compensate. This led to the last line, which has eof instead of newline, to have one less than actual. (me dumb)

--- Ch1/1-16/1-16.c
+++ Ch1/1-16/1-16.c
@@ -1,47 +1,61 @@
 /**
- * 1-16: getLineWithLength function that prints longest line + line count
+ * 1-16: getLenOfLine function that prints longest line + line count
  */
 
 #include <stdio.h>
-#define MAXLINE 1000
 
-int getLineWithLength(FILE *fp, char line[], int maxline);
+#define MAXLINE 1000
+#define ERROR -1
+#define OK 0
+
+int openFile(FILE **fp);
+int getLenOfLine(FILE *fp, char line[], int maxline);
 void copyLongest(char to[], char from[]);
 
 int main() {
-	int len;
-	int max;
+	int len, max;
+	int fileError;
 	char line[MAXLINE];
 	char longest[MAXLINE];
-	max = 0;
-	FILE *fp = fopen("T:\\Take\\CProgrammingLanguageRnK2ndEd\\Ch1\\1-16\\test.txt", "r");
-	if (fp == NULL) {
-		perror("wow!");
-		return -1;
+	FILE *fp;
+	len = max = 0;
+	fileError = 0;
+	for (int i = 0; i < MAXLINE; i++) {
+		line[i] = 0;
+		longest[i] = 0;
 	}
-	while ((len = getLineWithLength(fp, line, MAXLINE)) > 0)
+
+	fileError = openFile(&fp);
+	if (fileError == ERROR)
+		return ERROR;
+	while ((len = getLenOfLine(fp, line, MAXLINE)) > 0)
 		if (len > max) {
 			max = len;
 			copyLongest(longest, line);
 		}
-	if (max > 0) /* there was a line */
-		printf("max leng = %d: %s", max, longest);
+	if (max > 0)
+		printf("max leng = %d: %s", max, longest); //max - 1 due to \n
 	fclose(fp);
 	return 0;
 }
 
+int openFile(FILE **fp) {
+	*fp = fopen("T:\\Take\\CProgrammingLanguageRnK2ndEd\\Ch1\\1-16\\test.txt", "r");
+	if (*fp == NULL) {
+		perror("wow!");
+		return ERROR;
+	}
+	return OK;
+}
 
-int getLineWithLength(FILE* fp, char s[], int lim) {
+int getLenOfLine(FILE *fp, char s[], int lim) {
 	int c, i;
 	c = i = 0;
 
 	for (i = 0; (i < lim - 1) && ((c = fgetc(fp)) != EOF) && (c != '\n'); ++i) {
 		s[i] = c;
 	}
-	if (c == '\n') {
-		s[i] = c;
-		++i;
-	}
+
 	s[i] = '\0';
 	return i;
 }
---
+++ Ch1/1-17/1-17.c
@@ -0,0 +1,59 @@
+/**
+ * prints all input line (from file) that are longer than 80 characters.
+ */
+
+#include <stdio.h>
+
+#define MAXLINE 1000
+#define ERROR -1
+#define OK 0
+
+int openFile(FILE **fp);
+int determineLineLen(FILE **fp, char currLine[]);
+void printLineLen(int len, char currLine[]);
+
+int main() {
+	int len = 0;
+	int fileError = 0;
+	FILE *fp = NULL;
+	char currLine[MAXLINE];
+	for (int i = 0; i < MAXLINE; i++)
+		currLine[i] = 0;
+
+	fileError = openFile(&fp);
+	if (fileError == ERROR) {
+		return ERROR;
+	}
+	while ((len = determineLineLen(&fp, currLine)) > 0) {
+		if (len > 80) {
+			printLineLen(len, currLine);
+		}
+	}
+	fclose(fp);
+	return OK;
+}
+
+int openFile(FILE **fp) {
+	*fp = fopen("T:\\Take\\CProgrammingLanguageRnK2ndEd\\Ch1\\1-17\\test.txt", "r");
+	if (*fp == NULL) {
+		perror("ROLO");
+		getchar();
+		return ERROR;
+	}
+	return OK;
+}
+
+int determineLineLen(FILE **fp, char currLine[]) {
+	int len, c;
+	len = c = 0;
+	for (len = 0; ((c = fgetc(*fp)) != EOF) && (c != '\n') && (len < MAXLINE); len++)
+		currLine[len] = (char)c;
+	currLine[len] = '\0';
+	return len;
+}
+
+void printLineLen(int len, char currLine[]) {
+	fflush(stdout);
+	printf("\nlen=%d: %s", len, currLine);
+	return;
+}
