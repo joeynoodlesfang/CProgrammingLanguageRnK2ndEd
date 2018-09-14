26b1e7c79a31181dfac498dcad8dc48b5d401498
Joe <zhoufang29@gmail.com>
2018-09-14 14:33:59 -0700

    1-21

--- Ch1/1-21/1-21.c
+++ Ch1/1-21/1-21.c
@@ -6,35 +6,63 @@
  *			preference?" I'm guessing space because (hopefully I'll code it this way), anything less than 8 will not
  *			trigger a tab replacement (ie slightly less of the code to go through)
  *
- * notables			getchar
+ * notables			getchar, structs,
  * modifications	will not write entab, since short single-purpose code can be used
+ * 					not accounting for a trillion spaces
  */
 
 #include <stdio.h>
 
-int spaces = 0;
-int tabs = 0;
+struct spaceTabs {
+	int spaces, tabs;
+};
 
-int calcNumSpacesAndTabs (int currPos, int spaceCounter);
+struct spaceTabs calcNumSpacesAndTabs (int currPos, int spaceCounter);
 
 int main(void) {
 	int posCounter = 0;
 	int spaceCounter = 0;
+	struct spaceTabs spaceTab = {0};
 	int c = 0;
 	while ((c = getchar()) != EOF) {
-		if (c == ' ') {
+		if (c == '\n' || c == EOF) {
+			printf("%c", c);
+			posCounter = 0;
+			spaceCounter = 0;
+		} else if (c == ' ') {
 			spaceCounter++;
 		} else {
 			if (spaceCounter != 0) {
-				calcNumSpacesAndTabs();
-
+				spaceTab = calcNumSpacesAndTabs(posCounter, spaceCounter);
+				posCounter+=spaceCounter;
+				for (int i = 0; i < spaceTab.tabs; i++) {
+					printf("\t");
+				}
+				for (int i = 0; i < spaceTab.spaces; i++) {
+					printf(" ");
+				}
 			}
+			printf("%c", c);
+			posCounter++;
+			spaceCounter = 0;
 		}
+		fflush(stdout);
 	}
 	return 0;
 }
 
-void calcNumSpacesAndTabs (int currPos, int spaceCounter) {
-	int numSpaces = 8-currPos%8;
-
+struct spaceTabs calcNumSpacesAndTabs (int currPos, int _spaceCounter) {
+	struct spaceTabs _spaceTab = {0};
+	int numSpacesIfTab = 8-currPos%8;
+	if (_spaceCounter < numSpacesIfTab) {
+		_spaceTab.spaces = _spaceCounter;
+	} else if (_spaceCounter == numSpacesIfTab) {
+		_spaceTab.tabs = 1;
+	} else {
+		_spaceCounter = _spaceCounter - numSpacesIfTab;
+		_spaceTab.tabs = 1;
+		_spaceTab.tabs += _spaceCounter/8;
+		_spaceTab.spaces = _spaceCounter%8;
+	}
+	return _spaceTab;
 }
